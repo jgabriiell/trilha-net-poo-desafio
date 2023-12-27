0e4d33b84236a8e171458213d7a45ef8b6a56df5
João Gabriel <jgabriiell33@gmail.com>
2023-12-27 18:25:39 -0300

    Projeto Poo concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("68745", "Modelo 1", "99999", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("YouTube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("84563", "Modelo 2", "77777", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
